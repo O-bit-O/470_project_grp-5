fbdca980dd7378c39abaec103cec3ec9a1e0f500
Tamzid Islam <tamzidislammahian@gmail.com>
2023-06-26 21:23:25 +0600

    Sprint 1

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src="https://www.shutterstock.com/image-photo/tasty-pork-buger-on-wooden-260nw-734784787.jpg" className="logo react" alt="React logo" />
-        </a>
+        {/* <a href="https://vitejs.dev" target="_blank"> */}
+        <img src="https://png.pngtree.com/png-clipart/20230325/original/pngtree-juicy-burgers-with-a-transparent-background-png-image_9002761.png" className="logo" alt="Vite logo" />
+        {/* </a> */}
+        {/* <a href="https://react.dev" target="_blank"> */}
+        <img src="https://www.cicis.com/content/images/cicis/Jalapeno%20pizza.png" className="logo react" alt="React logo" />
+        {/* </a> */}
       </div>
-      <h1>Food Delivery Site</h1>
-      <div className="card">
+      <h1>Food Delivery System</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
